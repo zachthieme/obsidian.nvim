cf63d9d96e4f2d40d72c8c8396feef7da3e6cd74
Zach Thieme <zach@techsage.org>
2025-04-24 22:48:41 -0700

    minor tweak

--- lua/obsidian/util.lua
+++ lua/obsidian/util.lua
@@ -530,7 +530,8 @@ util.toggle_checkbox = function(opts, line_num)
         -- local config = require("obsidian.config").get()
         -- if config.todo_comments then
         if checkboxes[i + 1] == "x" or checkboxes[i + 1] == "c" then
-          if not line:find "<!-- completed:" then
+          -- stylua: ignore
+            if not line:find("<!-- completed:") then
             line = line .. " <!-- completed:" .. os.date "%Y-%m-%d" .. " -->"
           end
         else
