d7f9ecfd889b8cc6aa36ecfdb73228440d7755ce
Zach Thieme <zach@techsage.org>
2025-04-27 20:00:45 -0700

    minor tweaksss

--- lua/obsidian/util.lua
+++ lua/obsidian/util.lua
@@ -534,7 +534,7 @@ util.toggle_checkbox = function(opts, line_num)
             line = line .. " <!-- completed:" .. os.date "%Y-%m-%d" .. " -->"
           end
         else
-          line = (line:gsub("%s*<!--%scompleted:.-%s-->", ""))
+          line = (line:gsub("%s*<!--%s*completed:.-%s*-->", ""))
         end
         -- end
         break
